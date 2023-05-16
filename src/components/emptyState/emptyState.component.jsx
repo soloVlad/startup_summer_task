ecc3b4573342b165b327a7130147e57232ca960c
Vlad Solovey <connectwithvlad@gmail.com>
2023-05-16 15:13:25 +0300

    feat: add redirect on click in EmptyState

--- src/components/emptyState/emptyState.component.jsx
+++ src/components/emptyState/emptyState.component.jsx
@@ -3,15 +3,29 @@ import { Button, Stack, Text, rem } from "@mantine/core";
 import { ReactComponent as EmptyStateIcon } from '../../assets/icons/guyWithSearch.svg';
 
 import useStyles from "./emptyState.styles";
+import { useNavigate } from "react-router-dom";
+import ROUTES from "../../constants/routes";
 
 const EmptyState = () => {
+  const navigate = useNavigate();
+
   const { classes } = useStyles();
+
+  const handleClick = () => {
+    navigate(ROUTES.HOME);
+  };
 
   return (
     <Stack spacing={rem(32)} align='center' className={classes.wrapper}>
       <EmptyStateIcon />
       <Text className={classes.text}>Упс, здесь еще ничего нет!</Text>
-      <Button variant='outline' className={classes.button}>Поиск Вакансий</Button>
+      <Button
+        variant='outline'
+        className={classes.button}
+        onClick={handleClick}
+      >
+        Поиск Вакансий
+      </Button>
     </Stack>
   );
 };
